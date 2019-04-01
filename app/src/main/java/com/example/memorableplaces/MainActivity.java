13e1248af84add6bee315f6fb5c69336714fe3fb
ritik22agg <ritikaggarwal.dtu@gmail.com>
2019-04-01 21:57:36 +0530

    Initial commit

---
+++ app/src/main/java/com/example/memorableplaces/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.memorableplaces;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Add Places....");
+        list.add("Hello");
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, list);
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if(position == 0){
+                    
+                }
+            }
+        });
+    }
+}
